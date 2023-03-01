069091c004c4659c4307e7b173b643eaf3a77480
Maksim Mordvintsev <mordvintsevmv@gmail.com>
2023-03-01 19:58:50 +0400

    Adding Cart to Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,6 +5,9 @@ import {DarkMode, LightMode} from "@mui/icons-material";
 import {Stack} from "@mui/material";
 import {useTypedSelector} from "../../hooks/typedHooks";
 import {useActions} from "../../hooks/actions";
+import {NavLink} from "react-router-dom";
+import cart from "../../img/cart.svg";
+import heart from "../../img/heart.svg";
 
 interface HeaderProps {
     title?: string
@@ -20,17 +23,26 @@ const Header: FC<HeaderProps> = ({title = "Marketplace"}) => {
     }
 
     return (
-        <div className={"header"}>
+        <div className={`header header-${theme}`}>
             <h1>{title}</h1>
 
             <div className={"header-buttons"}>
                 <Stack direction={"row"}>
+
+                    <NavLink to={"/cart"}>
+                        <img src={cart} alt={"cart"} className={"sidebar-nav__img"}/>
+                    </NavLink>
+
+                    <NavLink to={"/favorite"}>
+                        <img src={heart} alt={"favorite"} className={"sidebar-nav__img"}/>
+                    </NavLink>
 
                     <span onClick={changeThemeHandler}>
                         <IconButton>
                             {theme === "dark" ? <LightMode  color={"info"}/> : <DarkMode />}
                         </IconButton>
                     </span>
+
                 </Stack>
             </div>
 
--- src/components/Sidebar/SideNav/SideNav.tsx
+++ src/components/Sidebar/SideNav/SideNav.tsx
@@ -3,7 +3,6 @@ import {NavLink} from "react-router-dom";
 import home from "../../../img/home.svg"
 import list from "../../../img/list.svg"
 import table from "../../../img/table.svg"
-import heart from "../../../img/heart.svg"
 import categories from "../../../img/categories.svg"
 import {useTypedSelector} from "../../../hooks/typedHooks";
 
@@ -27,11 +26,6 @@ const SideNav: FC = () => {
                 <NavLink to={"products"}
                          className={({isActive}) => isActive ? 'sidebar-nav__active-link sidebar-nav__link' : 'sidebar-nav__link'}>
                     <img src={list} alt={"list"} className={"sidebar-nav__img"}/>
-                </NavLink>
-
-                <NavLink to={"favorite"}
-                         className={({isActive}) => isActive ? 'sidebar-nav__active-link sidebar-nav__link' : 'sidebar-nav__link'}>
-                    <img src={heart} alt={"favorite"} className={"sidebar-nav__img"}/>
                 </NavLink>
 
                 <NavLink to={"categories"}
