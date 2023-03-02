ea558cacb09dd09324ca5cfdbc16f6243f212145
Maksim Mordvintsev <mordvintsevmv@gmail.com>
2023-03-02 17:09:22 +0400

    Fixing bug: zero element ([0]) on empty localStorage

--- src/redux/actions/productActions.ts
+++ src/redux/actions/productActions.ts
@@ -18,25 +18,46 @@ export const fetchProducts = () => async (dispatch: Dispatch) => {
 export const setCart = (cart: number[]) => (dispatch: Dispatch) => {
     localStorage.setItem('cart', cart.toString())
 
-    let cartList = localStorage.getItem('cart')?.split(",").map(Number) || []
-    dispatch(updateCart(cartList))
+    let cartList = localStorage.getItem('cart')
+
+    if (cartList !== null && cartList !== ''){
+        dispatch(updateCart(cartList.split(",").map(Number)))
+    } else{
+        dispatch(updateCart([]))
+    }
 
 }
 
 export const setFavourite = (favorite: number[]) => (dispatch: Dispatch) => {
     localStorage.setItem('favorite', favorite.toString())
 
-    let favoriteList = localStorage.getItem('favorite')?.split(",").map(Number) || []
-    dispatch(updateFavorite(favoriteList))
+    let favoriteList = localStorage.getItem('favorite')
+
+    if (favoriteList !== null && favoriteList !== '') {
+        dispatch(updateFavorite(favoriteList.split(",").map(Number)))
+    } else {
+        dispatch(updateFavorite([]))
+    }
 }
 
 export const fetchLists = () => (dispatch: Dispatch) => {
 
-    let favoriteList = localStorage.getItem('favorite')?.split(",").map(Number) || []
-    dispatch(updateFavorite(favoriteList))
 
-    let cartList = localStorage.getItem('cart')?.split(",").map(Number) || []
-    dispatch(updateCart(cartList))
+    let favoriteList = localStorage.getItem('favorite')
 
+    if (favoriteList !== null && favoriteList !== '') {
+        dispatch(updateFavorite(favoriteList.split(",").map(Number)))
+    } else {
+        dispatch(updateFavorite([]))
+    }
+
+
+    let cartList = localStorage.getItem('cart')
+
+    if (cartList !== null && cartList !== ''){
+        dispatch(updateCart(cartList.split(",").map(Number)))
+    } else{
+        dispatch(updateCart([]))
+    }
 
 }
