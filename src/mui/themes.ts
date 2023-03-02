c4f213c20b4a7d392cadbfd7c5c1b826d4cefb71
Maksim Mordvintsev <mordvintsevmv@gmail.com>
2023-03-02 18:12:16 +0400

    Updating theme style

--- src/mui/themes.ts
+++ src/mui/themes.ts
@@ -5,15 +5,22 @@ export const lightTheme: ThemeOptions = {
         fontFamily: 'Montserrat, Arial',
     },
     palette: {
-        mode: "light"
+        mode: "light",
+        secondary: {
+            main: '#EA6185',
+            contrastText: '#fff',
+        }
     },
 };
-
 export const darkTheme: ThemeOptions = {
     typography: {
         fontFamily: 'Montserrat, Arial',
     },
     palette: {
         mode: "dark",
+        secondary: {
+            main: '#EA8585',
+            contrastText: '#fff',
+        }
     },
 };
