3b4bfd26cb0be7c174add50f3f3efd03fd1ae082
Maksim Mordvintsev <mordvintsevmv@gmail.com>
2023-03-02 19:18:50 +0400

    Fixing fonts

--- src/mui/themes.ts
+++ src/mui/themes.ts
@@ -3,6 +3,43 @@ import {ThemeOptions} from "@mui/material/styles";
 export const lightTheme: ThemeOptions = {
     typography: {
         fontFamily: 'Montserrat, Arial',
+        fontSize: 14,
+        h1: {
+            fontSize: "50px",
+            fontWeight: "500",
+            lineHeight: "70px",
+            color: "rgba(50, 50, 50, .9)"
+        },
+        h2: {
+            fontSize: "24px",
+            fontWeight: "500",
+            lineHeight: "36px",
+            color: "rgba(50, 50, 50, .9)"
+        },
+        h3: {
+            fontSize: "22px",
+            fontWeight: "500",
+            lineHeight: "33px",
+            color: "rgba(50, 50, 50, .9)"
+        },
+        h4: {
+            fontSize: "20px",
+            fontWeight: "500",
+            lineHeight: "30px",
+            color: "rgba(50, 50, 50, .9)"
+        },
+        h5: {
+            fontSize: "18px",
+            fontWeight: "500",
+            lineHeight: "30px",
+            color: "rgba(50, 50, 50, .55)"
+        },
+        h6: {
+            fontSize: "16px",
+            fontWeight: "500",
+            lineHeight: "24px",
+            color: "rgba(50, 50, 50, .55)"
+        },
     },
     palette: {
         mode: "light",
@@ -15,6 +52,42 @@ export const lightTheme: ThemeOptions = {
 export const darkTheme: ThemeOptions = {
     typography: {
         fontFamily: 'Montserrat, Arial',
+        h1: {
+            fontSize: "50px",
+            fontWeight: "500",
+            lineHeight: "70px",
+            color: "rgba(255, 255, 255, .9)"
+        },
+        h2: {
+            fontSize: "24px",
+            fontWeight: "500",
+            lineHeight: "36px",
+            color: "rgba(255, 255, 255, .9)"
+        },
+        h3: {
+            fontSize: "22px",
+            fontWeight: "500",
+            lineHeight: "33px",
+            color: "rgba(255, 255, 255, .9)"
+        },
+        h4: {
+            fontSize: "20px",
+            fontWeight: "500",
+            lineHeight: "30px",
+            color: "rgba(255, 255, 255, .9)"
+        },
+        h5: {
+            fontSize: "18px",
+            fontWeight: "500",
+            lineHeight: "30px",
+            color: "rgba(255, 255, 255, .55)"
+        },
+        h6: {
+            fontSize: "16px",
+            fontWeight: "500",
+            lineHeight: "24px",
+            color: "rgba(255, 255, 255, .9)"
+        },
     },
     palette: {
         mode: "dark",
