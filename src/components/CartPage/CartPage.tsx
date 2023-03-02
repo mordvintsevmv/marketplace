c3328715d4b3aee59baca1921d4a2a838f530a5e
Maksim Mordvintsev <mordvintsevmv@gmail.com>
2023-03-02 20:00:42 +0400

    Adding sum for cart

--- src/components/CartPage/CartPage.tsx
+++ src/components/CartPage/CartPage.tsx
@@ -2,16 +2,21 @@ import React, {FC} from "react";
 import {useTypedSelector} from "../../hooks/typedHooks";
 import ProductCard from "../ProductCard/ProductCard";
 import Header from "../Header/Header";
+import './CartPage.scss'
+import Button from '@mui/material/Button';
+import {Stack, Typography} from "@mui/material";
 
 const CartPage: FC = () => {
     const {cart, products} = useTypedSelector(state => state.productReducer)
 
     let cartCards: Array<React.ReactNode>
+    let sum = 0
 
     if (cart.length !== 0 && products.length !== 0){
         cartCards = cart.map(favID => {
             const productID = products.findIndex(product => product.id === favID)
             const product = products[productID]
+            sum += product.price
             return (<ProductCard product={product} key={product.id}/>)
         })
     } else {
@@ -22,9 +27,23 @@ const CartPage: FC = () => {
         <div className={"cart-page"}>
             <Header title={"Cart Items"}/>
 
-            <div className={"cards-adaptive"}>
+            <div className={"cards-adaptive cart-page__cards"}>
                 {cartCards}
             </div>
+
+            <Stack direction={'row'} spacing={2} justifyContent="center" alignItems="center">
+                <Button variant="contained" disabled>
+                    Order
+                </Button>
+
+                <Typography variant={'h3'} color={'secondary'}>
+                    ${sum}
+                </Typography>
+            </Stack>
+
+
+
+
         </div>
     )
 
