63ae4eb34935f93fa69d371b4cded3259720582e
Maksim Mordvintsev <mordvintsevmv@gmail.com>
2023-03-02 17:24:26 +0400

    Creating Themes for MUI

---
+++ src/mui/themes.ts
@@ -0,0 +1,19 @@
+import {ThemeOptions} from "@mui/material/styles";
+
+export const lightTheme: ThemeOptions = {
+    typography: {
+        fontFamily: 'Montserrat, Arial',
+    },
+    palette: {
+        mode: "light"
+    },
+};
+
+export const darkTheme: ThemeOptions = {
+    typography: {
+        fontFamily: 'Montserrat, Arial',
+    },
+    palette: {
+        mode: "dark",
+    },
+};
