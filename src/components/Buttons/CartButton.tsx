26b7336d5df96adeefc2240421ad1a8e0b9d6c0b
Maksim Mordvintsev <mordvintsevmv@gmail.com>
2023-03-03 17:36:31 +0400

    Creating new style for cart button

--- src/components/Buttons/CartButton.tsx
+++ src/components/Buttons/CartButton.tsx
@@ -4,12 +4,14 @@ import {useTypedSelector} from "../../hooks/typedHooks";
 import {useActions} from "../../hooks/actions";
 import ShoppingBasketOutlinedIcon from '@mui/icons-material/ShoppingBasketOutlined';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import {Button} from "@mui/material";
 
 interface CartButtonProps {
-    productID: number
+    productID: number,
+    type?: string
 }
 
-const CartButton: FC<CartButtonProps> = ({productID}) => {
+const CartButton: FC<CartButtonProps> = ({productID, type = 'icon'}) => {
 
     const [cartFlag, setCartFlag] = useState(false)
     const {cart} = useTypedSelector(state => state.productReducer)
@@ -39,13 +41,23 @@ const CartButton: FC<CartButtonProps> = ({productID}) => {
         checkInCart();
     }, [cart])
 
-    return (
-        <span onClick={CartHandler}>
+    if (type === 'button'){
+        return (
+            <span onClick={CartHandler}>
+                <Button variant="outlined" startIcon={<ShoppingBasketIcon/>} color={cartFlag ? 'secondary' : 'primary'}>
+                    {cartFlag ? 'remove from cart' : 'add to cart'}
+                </Button>
+        </span>)
+    } else {
+        return (
+            <span onClick={CartHandler}>
             <IconButton aria-label={"Add to cart"}>
                 {cartFlag ? <ShoppingBasketIcon color={'secondary'}/> : <ShoppingBasketOutlinedIcon/>}
             </IconButton>
         </span>
-    )
+        )
+    }
+
 }
 
 export default CartButton
